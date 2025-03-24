d111179e21f7f0fc914655f75ac78aa1322d4c6c
RJOHNSON005 <rjohnson005@vplinacad29.ad.csbsju.edu>
2025-03-23 21:02:41 -0500

    Worked on Admin, Account, and User classes

--- src/cmc/backend/User.java
+++ src/cmc/backend/User.java
@@ -1,106 +1,33 @@
 package cmc.backend;
 
-public class User {
-	public String username;
-	public String password;
-	public char type; // u or a
-	public String firstName;
-	public String lastName;
-	public char activated; // Y or N
+import java.util.List;
 
-	public User(String username, String password, char type, String firstName,
-			String lastName) {
-		this.username = username;
-		this.password = password;
-		this.type = type;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.activated = 'Y'; // users always start activated
-	}
+public class User{
 	
+	private List<University> savedSchoolList;
+
 	/**
-	 * @return the username
+	 * @param savedSchoolList
 	 */
-	public String getUsername() {
-		// NOTE: we cannot modify the username, since this is the core
-		// "id" for users in the database (and must be unique)
-		return username;
+	public User(List<University> savedSchoolList) {
+		super();
+		this.savedSchoolList = savedSchoolList;
 	}
 
 	/**
-	 * @return the password
+	 * @return the savedSchoolList
 	 */
-	public String getPassword() {
-		return password;
+	public List<University> getSavedSchoolList() {
+		return savedSchoolList;
 	}
 
 	/**
-	 * @param password the password to set
+	 * @param savedSchoolList the savedSchoolList to set
 	 */
-	public void setPassword(String password) {
-		this.password = password;
+	public void setSavedSchoolList(List<University> savedSchoolList) {
+		this.savedSchoolList = savedSchoolList;
 	}
 
-	/**
-	 * @return the type
-	 */
-	public char getType() {
-		return type;
-	}
 
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(char type) {
-		this.type = type;
-	}
 	
-	// return true if the user is an admin ('a') type,
-	// or false otherwise
-	public boolean isAdmin() {
-		return (type == 'a');
-	}
-
-	/**
-	 * @return the firstName
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
-
-	/**
-	 * @param firstName the firstName to set
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	/**
-	 * @return the lastName
-	 */
-	public String getLastName() {
-		return lastName;
-	}
-
-	/**
-	 * @param lastName the lastName to set
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	/**
-	 * @return the activated
-	 */
-	public char getActivated() {
-		return activated;
-	}
-
-	/**
-	 * @param activated the activated to set
-	 */
-	public void setActivated(char activated) {
-		this.activated = activated;
-	}
-
 }
