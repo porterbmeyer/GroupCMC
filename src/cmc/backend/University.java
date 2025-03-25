448c4903aff298fcfa511d77a3a0f8d30102fa94
RJOHNSON005 <rjohnson005@vplinacad06.ad.csbsju.edu>
2025-03-24 21:03:57 -0500

    Added attributes for University

--- src/cmc/backend/University.java
+++ src/cmc/backend/University.java
@@ -7,7 +7,187 @@ public class University {
 	private String state;
 	private int population;
 	private double tuition;
+	private String control;
+	private double percentFemale;
+	private int satVerbal;
+	private int satMath;
+	private double expenses;
+	private double percentFinancialAid;
+	private int numberApplicants;
+	private double acceptanceRate;
+	private double enrollmentRate;
+	private int academicScale;
+	private int socialScale;
+	private int qualityScale;
 	
+	/**
+	 * @return the qualityScale
+	 */
+	public int getQualityScale() {
+		return qualityScale;
+	}
+
+	/**
+	 * @param qualityScale the qualityScale to set
+	 */
+	public void setQualityScale(int qualityScale) {
+		this.qualityScale = qualityScale;
+	}
+
+	/**
+	 * @return the control
+	 */
+	public String getControl() {
+		return control;
+	}
+
+	/**
+	 * @param control the control to set
+	 */
+	public void setControl(String control) {
+		this.control = control;
+	}
+
+	/**
+	 * @return the percentFemale
+	 */
+	public double getPercentFemale() {
+		return percentFemale;
+	}
+
+	/**
+	 * @param percentFemale the percentFemale to set
+	 */
+	public void setPercentFemale(double percentFemale) {
+		this.percentFemale = percentFemale;
+	}
+
+	/**
+	 * @return the satVerbal
+	 */
+	public int getSatVerbal() {
+		return satVerbal;
+	}
+
+	/**
+	 * @param satVerbal the satVerbal to set
+	 */
+	public void setSatVerbal(int satVerbal) {
+		this.satVerbal = satVerbal;
+	}
+
+	/**
+	 * @return the satMath
+	 */
+	public int getSatMath() {
+		return satMath;
+	}
+
+	/**
+	 * @param satMath the satMath to set
+	 */
+	public void setSatMath(int satMath) {
+		this.satMath = satMath;
+	}
+
+	/**
+	 * @return the expenses
+	 */
+	public double getExpenses() {
+		return expenses;
+	}
+
+	/**
+	 * @param expenses the expenses to set
+	 */
+	public void setExpenses(double expenses) {
+		this.expenses = expenses;
+	}
+
+	/**
+	 * @return the percentFinancialAid
+	 */
+	public double getPercentFinancialAid() {
+		return percentFinancialAid;
+	}
+
+	/**
+	 * @param percentFinancialAid the percentFinancialAid to set
+	 */
+	public void setPercentFinancialAid(double percentFinancialAid) {
+		this.percentFinancialAid = percentFinancialAid;
+	}
+
+	/**
+	 * @return the numberApplicants
+	 */
+	public int getNumberApplicants() {
+		return numberApplicants;
+	}
+
+	/**
+	 * @param numberApplicants the numberApplicants to set
+	 */
+	public void setNumberApplicants(int numberApplicants) {
+		this.numberApplicants = numberApplicants;
+	}
+
+	/**
+	 * @return the acceptanceRate
+	 */
+	public double getAcceptanceRate() {
+		return acceptanceRate;
+	}
+
+	/**
+	 * @param acceptanceRate the acceptanceRate to set
+	 */
+	public void setAcceptanceRate(double acceptanceRate) {
+		this.acceptanceRate = acceptanceRate;
+	}
+
+	/**
+	 * @return the enrollmentRate
+	 */
+	public double getEnrollmentRate() {
+		return enrollmentRate;
+	}
+
+	/**
+	 * @param enrollmentRate the enrollmentRate to set
+	 */
+	public void setEnrollmentRate(double enrollmentRate) {
+		this.enrollmentRate = enrollmentRate;
+	}
+
+	/**
+	 * @return the academicScale
+	 */
+	public int getAcademicScale() {
+		return academicScale;
+	}
+
+	/**
+	 * @param academicScale the academicScale to set
+	 */
+	public void setAcademicScale(int academicScale) {
+		this.academicScale = academicScale;
+	}
+
+	/**
+	 * @return the socialScale
+	 */
+	public int getSocialScale() {
+		return socialScale;
+	}
+
+	/**
+	 * @param socialScale the socialScale to set
+	 */
+	public void setSocialScale(int socialScale) {
+		this.socialScale = socialScale;
+	}
+
 	/**
 	 * @param name
 	 * @param location
