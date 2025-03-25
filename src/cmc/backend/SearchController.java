6896b37c58efcb042ef767a1a8917ffe5a13e8cd
DPERRILL001 <dperrill001@vplinacad32.ad.csbsju.edu>
2025-03-24 20:32:07 -0500

    Created SearchController file

--- src/cmc/backend/SearchController.java
+++ src/cmc/backend/SearchController.java
@@ -1,5 +1,12 @@
 package cmc.backend;
 
-public class SearchController {
+import dblibrary.project.csci230.UniversityDBLibrary;
 
+public class SearchController {
+	private UniversityDBLibrary database;
+
+	public SearchController() {
+		// TODO: we'll need to update this to our team's actual database someday!
+		this.database = new UniversityDBLibrary("csci230", "Csci230$");
+	}
 }
