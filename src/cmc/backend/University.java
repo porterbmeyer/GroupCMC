7a1dd274f637ee4f2ddee17bef7b1a235fa74bb2
RJOHNSON005 <rjohnson005@vplinacad06.ad.csbsju.edu>
2025-03-24 21:33:39 -0500

    University constructor added

--- src/cmc/backend/University.java
+++ src/cmc/backend/University.java
@@ -20,6 +20,55 @@ public class University {
 	private int socialScale;
 	private int qualityScale;
 	
+	
+	
+	/**
+	 * @param name
+	 * @param location
+	 * @param state
+	 * @param population
+	 * @param tuition
+	 * @param control
+	 * @param percentFemale
+	 * @param satVerbal
+	 * @param satMath
+	 * @param expenses
+	 * @param percentFinancialAid
+	 * @param numberApplicants
+	 * @param acceptanceRate
+	 * @param enrollmentRate
+	 * @param academicScale
+	 * @param socialScale
+	 * @param qualityScale
+	 */
+
+	
+	/** This is a test comment
+	 */
+	public University(String name, String location, String state, int population, double tuition, String control,
+			double percentFemale, int satVerbal, int satMath, double expenses, double percentFinancialAid,
+			int numberApplicants, double acceptanceRate, double enrollmentRate, int academicScale, int socialScale,
+			int qualityScale) {
+		super();
+		this.name = name;
+		this.location = location;
+		this.state = state;
+		this.population = population;
+		this.tuition = tuition;
+		this.control = control;
+		this.percentFemale = percentFemale;
+		this.satVerbal = satVerbal;
+		this.satMath = satMath;
+		this.expenses = expenses;
+		this.percentFinancialAid = percentFinancialAid;
+		this.numberApplicants = numberApplicants;
+		this.acceptanceRate = acceptanceRate;
+		this.enrollmentRate = enrollmentRate;
+		this.academicScale = academicScale;
+		this.socialScale = socialScale;
+		this.qualityScale = qualityScale;
+	}
+
 	/**
 	 * @return the qualityScale
 	 */
@@ -188,20 +237,7 @@ public class University {
 		this.socialScale = socialScale;
 	}
 
-	/**
-	 * @param name
-	 * @param location
-	 * @param state
-	 * @param population
-	 * @param tuition
-	 */
-	public University(String name, String location, String state, int population, double tuition) {
-		this.name = name;
-		this.location = location;
-		this.state = state;
-		this.population = population;
-		this.tuition = tuition;
-	}
+
 
 	/**
 	 * @return the name
