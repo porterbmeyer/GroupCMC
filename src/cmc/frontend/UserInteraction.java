c1c45b09094383478cb188777b196853adc5cb1b
PMEYER001 <pmeyer001@vplinacad17.ad.csbsju.edu>
2025-03-25 20:30:40 -0500

    updated interactions

--- src/cmc/frontend/UserInteraction.java
+++ src/cmc/frontend/UserInteraction.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 
-import cmc.backend.User;
 
 public class UserInteraction {
 	
@@ -57,10 +56,6 @@ public class UserInteraction {
 		}
 	}
 	
-	// for admins, this gets the list of all users in the system
-	public List<User> getAllUsers() {
-		return this.databaseController.getAllUsers();
-	}
 	
 	// ask the admin for details and then attempt to add a user to the
 	// database
@@ -78,7 +73,6 @@ public class UserInteraction {
 		if (s.nextLine().trim().equalsIgnoreCase("y"))
 			isAdmin = true;
 		
-		Account newAccount = new Account(firstName, lastName, username, password, isAdmin ? 'A' : 'U','Y'); 
 		return this.accountController.createAccount(username, password, firstName, lastName, isAdmin ? 'A' : 'U', 'Y');
 	}
 	
