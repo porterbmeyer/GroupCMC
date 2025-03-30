ff42fc9a85fbf1ebcf20b2c5d4956ae6954fff23
RMURPHY004 <rmurphy004@vplinacad11.ad.csbsju.edu>
2025-03-29 21:47:01 -0500

    white box test for account class

---
+++ src/cmc/backend/TestAccount.java
@@ -0,0 +1,7 @@
+package cmc.backend;
+
+public class TestAccount extends Account{
+public TestAccount(String firstname, String lastname, String username, String password, char type, char active) {
+    super(firstname, lastname, username, password, type, active);
+}
+}
