c0f3e04a6faf7fc457c947a1826a2ac3e2d6abd0
PMEYER001 <pmeyer001@vplinacad09.ad.csbsju.edu>
2025-04-08 14:20:53 -0500

    fixed deactivatebug test

--- test/regression/DeactivatedUserBugTest.java
+++ test/regression/DeactivatedUserBugTest.java
@@ -5,18 +5,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import cmc.backend.Account;
 import cmc.backend.DatabaseController;
-import cmc.backend.SystemController;
-import cmc.backend.User;
 import junit.framework.Assert;
 
+@SuppressWarnings("deprecation")
 public class DeactivatedUserBugTest {
 
 	@Before
 	public void setUp() throws Exception {
 		
 		  DatabaseController tester1 = new DatabaseController();
-		  tester1.addUser("username9947", "password1234322", 'u', "firstname","lastname");
+		  tester1.addUser("firstname", "lastname", "username9947", "password", 'U');
 		  tester1.deactivateUser("username9947");
 		}
 
@@ -27,10 +27,13 @@ public class DeactivatedUserBugTest {
 		  tester2.removeUser("username9947");
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void deactivatedUserTest() {
-	    SystemController teste = new SystemController();
-		User result = teste.login("username9947", "password1234322");
-		Assert.assertNull(result);
+		  DatabaseController tester3 = new DatabaseController();
+		  Account test = tester3.getUser("username9947");
+		  char tesr = test.getActive();
+		  
+		  Assert.assertEquals(tesr, 'N');
 	}
 }
