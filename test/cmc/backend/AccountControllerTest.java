dbc530cbc74d77054bf37b5cecb5a91a1fc753e3
PMEYER001 <pmeyer001@vplinacad04.ad.csbsju.edu>
2025-04-10 12:50:40 -0500

    made Uni/Account tests

---
+++ test/cmc/backend/AccountControllerTest.java
@@ -0,0 +1,49 @@
+package cmc.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateAccountDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangePassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditAccount() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/cmc/backend/UniversityControllerTest.java
@@ -0,0 +1,44 @@
+package cmc.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UniversityControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDeleteUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllUniversities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUniversityByName() {
+		fail("Not yet implemented");
+	}
+
+}
