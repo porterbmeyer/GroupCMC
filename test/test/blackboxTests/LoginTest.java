3be6a9801f06b873bfde412d25e9dc080e309e4b
PMEYER001 <pmeyer001@vplinacad11.ad.csbsju.edu>
2025-04-14 00:11:23 -0500

    login test

--- test/test/blackboxTests/LoginTest.java
+++ test/test/blackboxTests/LoginTest.java
@@ -2,11 +2,14 @@ package test.blackboxTests;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.io.StringReader;
 import cmc.backend.DatabaseController;
+import cmc.frontend.UserInteraction;
 
 /**
  * Unit tests for the login functionality in the application.
@@ -43,7 +46,29 @@ public class LoginTest {
      * Verifies that the system properly authenticates valid users.
      */
     @Test
-    public void loginTest() {
-        // Test implementation goes here
-    }
+    public void loginTest() throws Exception {
+    	    
+    	        DatabaseController dbcon = new DatabaseController();
+    	        UserInteraction loginController = new UserInteraction(); // Make sure this is your class with login()
+
+    	        // === Test: Valid login ===
+    	        Scanner validScanner = new Scanner(new StringReader("pohmann\npeterword\n"));
+    	        boolean validLogin = loginController.login(validScanner);
+    	        assertTrue("Login should succeed with correct credentials", validLogin);
+    	        validScanner.close();
+
+    	        // === Test: Invalid password ===
+    	        Scanner wrongPassScanner = new Scanner(new StringReader("pohmann\nwrongpass\n"));
+    	        boolean invalidPass = loginController.login(wrongPassScanner);
+    	        assertFalse("Login should fail with incorrect password", invalidPass);
+    	        wrongPassScanner.close();
+
+    	        // === Test: Non-existent user ===
+    	        Scanner invalidUserScanner = new Scanner(new StringReader("fakeuser\nfakepass\n"));
+    	        boolean invalidUser = loginController.login(invalidUserScanner);
+    	        assertFalse("Login should fail with non-existent user", invalidUser);
+    	        invalidUserScanner.close();
+    	    }
 }
+
+
