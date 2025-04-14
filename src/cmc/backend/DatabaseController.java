634f67ecd7cef608e2a15801984a3396c2b2775d
PMEYER001 <pmeyer001@vplinacad11.ad.csbsju.edu>
2025-04-14 00:34:22 -0500

    regression seach bug

--- src/cmc/backend/DatabaseController.java
+++ src/cmc/backend/DatabaseController.java
@@ -109,7 +109,7 @@ public class DatabaseController {
 		int result = this.database.user_saveSchool(username, schoolName);
 		if (result != 1) {
 			// TODO: How can we tell the difference?
-			throw new Error("Error saving school to user in the DB.  Already present?  DB error?");
+			return false;
 		}
 		else {
 			return true;
--- test/regression/AddSavedSchoolTest.java
+++ test/regression/AddSavedSchoolTest.java
@@ -1,13 +1,12 @@
 package regression;
 
-import static org.junit.Assert.*;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import cmc.backend.DatabaseController;
-import cmc.backend.SystemController;
 import junit.framework.Assert;
 
 public class AddSavedSchoolTest {
@@ -15,20 +14,21 @@ public class AddSavedSchoolTest {
 	@Before
 	public void setUp() throws Exception {
 		 DatabaseController tester1 = new DatabaseController();
-		 tester1.addUser("username99332", "password123432121", 'u', "firstname1","lastname1");
+		 tester1.addUser("firstname", "lastname", "username9947", "password", 'U');
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		 DatabaseController tester12 = new DatabaseController();
-		 tester12.removeUser("username99332");
+		 tester12.removeUser("username9947");
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void addSchoolTest() {
-		SystemController tess = new SystemController();
-		tess.saveSchool("username99332", "");
-		Assert.assertEquals(tess.saveSchool("username99332", ""), new IllegalArgumentException());
+		DatabaseController tess = new DatabaseController();
+		Boolean b = tess.saveSchool("username99332", "");
+		Assert.assertFalse(b);
 		
 	}
 
