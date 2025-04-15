9a0af486a6c09be35343a5d4df9f7e8a27842750
PMEYER001 <pmeyer001@vplinacad01.ad.csbsju.edu>
2025-04-14 22:08:36 -0500

    userschooltest

--- test/systemtest/UserSaveSchoolTest.java
+++ test/systemtest/UserSaveSchoolTest.java
@@ -6,35 +6,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import cmc.backend.Account;
 import cmc.backend.DatabaseController;
-import cmc.backend.User;
 import junit.framework.Assert;
 
 public class UserSaveSchoolTest {
 
 	@Before
 	public void setUp() throws Exception {
-		DatabaseController dbcon = new DatabaseController();
-		dbcon.addUser("firstname", "lastname", "Username1", "password", 'U');
-		}
+		 DatabaseController dbcon = new DatabaseController();
+	     dbcon.addUser("Peter", "Ohmann", "pohmann", "peterword", 'U');
+	}
 
 	@After
 	public void tearDown() throws Exception {
-		DatabaseController dbcon = new DatabaseController();
-		dbcon.removeUser("Username1");
+		 DatabaseController dbcon = new DatabaseController();
+	     dbcon.removeUser("pohmann");
 	}
 
-	@SuppressWarnings("deprecation")
 	@Test
-	public void testAddSavedSchool() {
-		DatabaseController dbcon = new DatabaseController();
+	public void SaveSchoolTest() {
+		DatabaseController dbcon = new DatabaseController();	
+		boolean pass = dbcon.saveSchool("pohmann", "BARD");
 		
-		boolean pass = dbcon.saveSchool("Username1", "BARD");
-		//test to save school pass
+		//test to pass added to saved list
 		Assert.assertTrue(pass);
-		//test to fail already in system
+		
+		//test to fail already in DB
 		Assert.assertFalse(dbcon.saveSchool("Username1","BARD"));
 		
+		//fail school is not in Database
+		Assert.assertFalse(dbcon.saveSchool("Username1","notaschool"));
 	}
+
 }
