27c45f0ab843247729b96acfd37a03416d194983
RMURPHY004 <rmurphy004@vplinacad21.ad.csbsju.edu>
2025-04-14 23:03:11 -0500

    added  UserTest to the testsuite

--- test/AllTests/AllTests.java
+++ test/AllTests/AllTests.java
@@ -8,9 +8,10 @@ import cmc.backend.AccountTest;
 import cmc.backend.DatabaseControllerTest;
 import cmc.backend.UniversityControllerTest;
 import cmc.backend.UniversityTest;
+import cmc.backend.UserTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({AccountControllerTest.class,AccountTest.class,DatabaseControllerTest.class, UniversityControllerTest.class,UniversityTest.class})
+@SuiteClasses({AccountControllerTest.class,AccountTest.class,DatabaseControllerTest.class, UniversityControllerTest.class,UniversityTest.class,UserTest.class})
 public class AllTests {
 
 }
